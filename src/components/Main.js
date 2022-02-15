761993c01f389cc92efd304506097589f82ff383
dotnotation <dotinmotionproductions@gmail.com>
2022-02-15 12:28:32 -0500

    starting to switch cladd Main into a functional component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Counter from './Counter';
 import List from './List';
 
-class Main extends React.Component {
-    state = {
-        darkMode: false
-    }
-
-    handleModeChange = () => {
-        this.setState(prevState => ({
-            darkMode: !prevState.darkMode
-        }))
-    }
-
-    render() {
-        return (
-            <div className={this.state.darkMode ? "main dark-mode" : "main light-mode"}>
-                <button onClick={this.handleModeChange}>{this.state.darkMode ? "Light Mode" : "Dark Mode"}</button>
-                <Counter />
-                <List />
-            </div>
-        )
-    }
+function Main(props){
+    return (
+        <p>Hi!</p>
+    )
 }
+// class Main extends React.Component {
+//     state = {
+//         darkMode: false
+//     }
+
+//     handleModeChange = () => {
+//         this.setState(prevState => ({
+//             darkMode: !prevState.darkMode
+//         }))
+//     }
+
+//     render() {
+//         return (
+//             <div className={this.state.darkMode ? "main dark-mode" : "main light-mode"}>
+//                 <button onClick={this.handleModeChange}>{this.state.darkMode ? "Light Mode" : "Dark Mode"}</button>
+//                 <Counter />
+//                 <List />
+//             </div>
+//         )
+//     }
+// }
 
 export default Main;
