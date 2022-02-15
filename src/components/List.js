00ea83a5770166750938c80b2cd3792bc0f42bbf
dotnotation <dotinmotionproductions@gmail.com>
2022-02-15 13:58:32 -0500

    changed List component into a functional component with hook

--- src/components/List.js
+++ src/components/List.js
@@ -1,41 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class List extends React.Component {
-    state = {
-        list: [],
-        item: ""
-    }
+function List(){
 
-    addToList = (e) => {
+    // const [state, setState] = useState({
+    //     list: [],
+    //     item: ""
+    // })
+    // one of two solutions but not recommended as you are passing in an object and it will be reset every time  
+
+    const [list, setList] = useState([])
+    
+    const [item, setItem] = useState("")
+    // it is recommended to set for each state
+
+    function addToList(e){
         e.preventDefault()
-        if (this.state.item) {
-            this.setState(prevState => ({
-                list: [...prevState.list, prevState.item],
-                item: ""
-            }))
+        if (item) {
+                setList(prevList => [...prevList, item])
+                setItem("")
         }
     }
 
-    handleChange = (e) => {
-        this.setState({
-            item: e.target.value
-        })
+    function handleChange(e){
+        setItem(e.target.value)
     }
-
-    render() { 
-        return (
-            <div className="left">
-                <h3>Your List</h3>
-                <ul>
-                    {this.state.list.map((item, i) => <li key={i}>{item}</li>)}
-                </ul>
-                <form onSubmit={this.addToList}>
-                    <input type="text" value={this.state.item} onChange={this.handleChange}/>
-                    <input type="submit" value="Add to List"/>
-                </form>
-            </div>
-        );
-    }
+    
+    return(
+        <div className="left">
+            <h3>Your List</h3>
+            <ul>
+                {list.map((item, i) => <li key={i}>{item}</li>)}
+            </ul>
+            <form onSubmit={addToList}>
+                <input type="text" value={item} onChange={handleChange}/>
+                <input type="submit" value="Add to List"/>
+            </form>
+        </div>
+    )
 }
+
+// class List extends React.Component {
+//     state = {
+//         list: [],
+//         item: ""
+//     }
+
+//     addToList = (e) => {
+//         e.preventDefault()
+//         if (this.state.item) {
+//             this.setState(prevState => ({
+//                 list: [...prevState.list, prevState.item],
+//                 item: ""
+//             }))
+//         }
+//     }
+
+//     handleChange = (e) => {
+//         this.setState({
+//             item: e.target.value
+//         })
+//     }
+
+//     render() { 
+//         return (
+//             <div className="left">
+//                 <h3>Your List</h3>
+//                 <ul>
+//                     {this.state.list.map((item, i) => <li key={i}>{item}</li>)}
+//                 </ul>
+//                 <form onSubmit={this.addToList}>
+//                     <input type="text" value={this.state.item} onChange={this.handleChange}/>
+//                     <input type="submit" value="Add to List"/>
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
  
 export default List;
