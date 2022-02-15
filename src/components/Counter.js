33a5c6f625f73d46fda4214b12b15c072c5ff8f9
dotnotation <dotinmotionproductions@gmail.com>
2022-02-15 13:30:27 -0500

    changed Counter component into a functional component using hooks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Counter extends React.Component {
-    state = {
-        total: 0
-    }
+function Counter(){
+    const [total, setTotal] = useState(0)
 
-    addToTotal = (e) => {
-        this.setState(prevState => ({
-            total: prevState.total + parseInt(e.target.dataset.add)
-        }))
+    function addToTotal(e){
+        // this.setState(prevState => ({
+        //     total: prevState.total + parseInt(e.target.dataset.add)
+        // }))
+        setTotal(prevTotal => prevTotal + parseInt(e.target.dataset.add))
+        // notice how the dataset in return is a string so that is why you need to parseInt
+        // no longer a method on the object which is why you convert the arrow function to a regular function
     }
-
-    render() { 
-        return (
-            <>
-                <p>Your total is: {this.state.total}</p>
-                <div>
-                    <button onClick={this.addToTotal} data-add="1">Add 1</button>
-                    <button onClick={this.addToTotal} data-add="3">Add 3</button>
-                    <button onClick={this.addToTotal} data-add="5">Add 5</button>
-                    <button onClick={this.addToTotal} data-add="7">Add 7</button>
-                </div>
-            </>
-        );
-    }
+    
+    return(
+        <>
+            <p>Your total is: {total}</p>
+            <div>
+                <button onClick={addToTotal} data-add="1">Add 1</button>
+                <button onClick={addToTotal} data-add="3">Add 3</button>
+                <button onClick={addToTotal} data-add="5">Add 5</button>
+                <button onClick={addToTotal} data-add="7">Add 7</button>
+            </div>
+        </>
+    )
 }
+
+// class Counter extends React.Component {
+//     state = {
+//         total: 0
+//     }
+
+//     addToTotal = (e) => {
+//         this.setState(prevState => ({
+//             total: prevState.total + parseInt(e.target.dataset.add)
+//         }))
+//     }
+
+//     render() { 
+//         return (
+//             <>
+//                 <p>Your total is: {this.state.total}</p>
+//                 <div>
+//                     <button onClick={this.addToTotal} data-add="1">Add 1</button>
+//                     <button onClick={this.addToTotal} data-add="3">Add 3</button>
+//                     <button onClick={this.addToTotal} data-add="5">Add 5</button>
+//                     <button onClick={this.addToTotal} data-add="7">Add 7</button>
+//                 </div>
+//             </>
+//         );
+//     }
+// }
  
 export default Counter;
